0fa57ca7eefd0bb619587269daf5f8b9dac55b76
Fabricio Braga <fabriciobrg@gmail.com>
2021-09-28 11:56:48 -0300

    Initial commit

---
+++ src/main/java/com/fabricio/projectmongodb/ProjectmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.fabricio.projectmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabricio/projectmongodb/ProjectmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabricio.projectmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
