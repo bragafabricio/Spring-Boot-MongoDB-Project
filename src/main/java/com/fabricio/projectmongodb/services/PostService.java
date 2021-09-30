aa1bfe4059ad90f0c5b2dc1b14fb01fbdd8b9936
Fabricio Braga <fabriciobrg@gmail.com>
2021-09-30 20:31:05 -0300

    Get post by Id

---
+++ src/main/java/com/fabricio/projectmongodb/services/PostService.java
@@ -0,0 +1,27 @@
+package com.fabricio.projectmongodb.services;
+
+import com.fabricio.projectmongodb.domain.Post;
+import com.fabricio.projectmongodb.domain.User;
+import com.fabricio.projectmongodb.dto.UserDTO;
+import com.fabricio.projectmongodb.repository.PostRepository;
+import com.fabricio.projectmongodb.repository.UserRepository;
+import com.fabricio.projectmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id){
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+
+}
